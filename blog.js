93495f7240067289920d05db63139ee9a00d5679
Scottie McGrath <scottiemcgrath@scottiemcgrath.ai>
2022-11-20 21:17:18 +0000

    Add files via upload

--- blog.js
+++ blog.js
@@ -3,6 +3,7 @@ let content = "ScoobyDoobyDoo"
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
+    console.log("function ran")
     mainFrame = document.getElementById("main-frame");
     let df = new DocumentFragment();
     let p = document.createElement("p");
