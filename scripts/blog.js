e083793bfb71706fdd9e99b4e22a1439f36bc9b1
Scottie McGrath <scottiemcgrath@scottiemcgrath.ai>
2022-11-21 18:28:41 +0000

    Add files via upload

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", init);
-var root = document.location.hostname
 
 function init(){
-    $("#main-frame").load(root+"blogposts/blogpost.html")
+    $("#main-frame").load("blogposts/blogpost.html")
 }
--- scripts/load-header.js
+++ scripts/load-header.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", init);
-var root = document.location.hostname
 
 function init(){
-    console.log(root)
     $("#header").load("general-html/header.html")
 }
